44fe59d2f60788dee582371d59fb1928ba8c76bb
Nate <ggn440@gmail.com>
2022-06-06 17:01:35 -0300

    Terminado

---
+++ index.js
@@ -0,0 +1,15 @@
+const Feli = ('Felicidades llegaste al dia ');
+let dia = parseInt(prompt('Ingresa un dia del 1 al 7'))
+    
+    while(dia != 7) {
+        alert(Feli + dia);
+        dia = prompt('Elige otro dia del 1 al 7');
+    }
+
+
+if (dia = 7) {
+    alert('ya no hay mas dias, adios')
+}
+    
+
+

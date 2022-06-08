249709a7065c4968c67867f5c56bcdbb54056a9b
Nate <ggn440@gmail.com>
2022-06-08 20:31:03 -0300

    Terminado

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
-const Feli = ('Felicidades llegaste al dia ');
-let dia = parseInt(prompt('Ingresa un dia del 1 al 7'))
-    
-    while(dia != 7) {
-        alert(Feli + dia);
-        dia = prompt('Elige otro dia del 1 al 7');
+function nivelFacil() {
+    const Pregunta = Number(prompt('¿Cuanto es 9x7?'));
+    while(Pregunta != 63 ){
+        const Pregunta2 = Number(prompt('Respuesta incorrecta, Sigue intentado ¿Cuanto es 9x7?'))
+        if (Pregunta2 == 63) {
+            alert('Respuesta correcta')
+            break;
+        }
     }
-
-
-if (dia = 7) {
-    alert('ya no hay mas dias, adios')
+    if (Pregunta == 63) {
+        alert('Respuesta correcta')
+    }
 }
-    
 
+function nivelIntermedio() {
+    const Pregunta = prompt('¿En qué continente esta japon?');
+    while(Pregunta != 'Asia'){
+        const Pregunta2 = prompt('Respuesta incorrecta, sigue intentando ¿En qué continente esta japon?')
+        if (Pregunta2 == 'Asia') {
+            alert('Respuesta correcta')
+            break;
+        }
+    }
+    if (Pregunta == 'Asia') {
+        alert('Respuesta correcta')
+    }
+}
+
+function nivelDificil() {
+    const Pregunta1 = Number(prompt('¿Cuanto es 10 + 10 x 10 + 10?'));
+    while(Pregunta1 != 120 ){
+        const Pregunta2 = Number(prompt('Respuesta incorrecta, sigue intentando ¿Cuanto es 10 + 10 x 10 + 10?'))
+        if (Pregunta2 == 120) {
+            alert('Respuesta correcta')
+            break;
+        }
+    }
+    if (Pregunta == 120) {
+        alert('Respuesta correcta')
+    }
+}
 
